d303a947ba02e75438c08289de867ae9291ee2d7
julianahrens <22180888+julianahrens@users.noreply.github.com>
2020-03-22 20:43:44 +0100

    feat: adding general site structure

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {DefaultComponent} from "./default.component";
 import {DashboardComponent} from "../../modules/dashboard/dashboard.component";
 import {RouterModule} from "@angular/router";
-
+import {SharedModule} from "../../shared/shared.module";
+import {MatSidenavModule} from "@angular/material/sidenav";
 
 
 @NgModule({
@@ -13,7 +14,10 @@ import {RouterModule} from "@angular/router";
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    SharedModule,
+    MatSidenavModule
   ]
 })
-export class DefaultModule { }
+export class DefaultModule {
+}
