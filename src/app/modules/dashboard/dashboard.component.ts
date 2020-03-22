4dbe3f56d627c6d673775776c39c83a20a446c69
julianahrens <22180888+julianahrens@users.noreply.github.com>
2020-03-22 20:03:05 +0100

    setup: general dashboard setup

---
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gms-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.sass']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
